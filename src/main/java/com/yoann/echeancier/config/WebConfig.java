03e224a180cd814a77f92fbd36fe33f204071f5b
ylaurent <laurent.yoann2900@gmail.com>
2025-08-18 19:50:54 +0200

    feat: correctif WebConfig

--- src/main/java/com/yoann/echeancier/config/WebConfig.java
+++ src/main/java/com/yoann/echeancier/config/WebConfig.java
@@ -29,7 +29,7 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:4200", "http://localhost:5173", "http://localhost:5174")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:4200", "http://localhost:5173", "http://localhost:5174", "https://echeancier-ejsi.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
